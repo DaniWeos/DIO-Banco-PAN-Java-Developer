641a7762802bbefef8e62aa023adb1aeaf41f6bc
Daniel Cruz <123400219+DaniWeos@users.noreply.github.com>
2023-03-14 15:29:09 -0300

    Delete estudo directory

--- estudo/estrutura-de-dados/ArvoreBinaria/ArvoreBinaria/src/ObjArvore.java
+++
@@ -1,9 +0,0 @@
-
-
-public abstract class ObjArvore <T> implements Comparable<T>{
-
-    public abstract boolean equals(Object o);
-    public abstract int hashCode();
-    public abstract int compareTo(T outro);
-    public abstract String toString();
-}
